e0396ee5cfca79c694610db772f50a43fa2a0a44
Aline Vieira <137254396+aliun1@users.noreply.github.com>
2025-11-26 22:54:00 -0300

    Atividade Finalizada

--- atividade_colaboradores.sql
+++ atividade_colaboradores.sql
@@ -12,6 +12,7 @@ CREATE TABLE tb_colaboradores (
     salario DECIMAL(10,2) NOT NULL,
     turno VARCHAR(15) NOT NULL
 );
+
 INSERT INTO tb_colaboradores (nome, setor, ramal, data_admissao, salario, turno)
 VALUES ('Jeferson Almeida', 'Administrativo', 509, '2024-10-05', 3000.00, 'Manhã');
 INSERT INTO tb_colaboradores (nome, setor, ramal, data_admissao, salario, turno)
@@ -29,7 +30,6 @@ WHERE salario > 2000;
 -- consulta salario menor que 2000
 SELECT * FROM tb_colaboradores
 WHERE salario < 2000;
-
 
 -- atualizacao do salario do colaborador de Id 4
 UPDATE tb_colaboradores
